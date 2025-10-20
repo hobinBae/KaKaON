e75d0b7027e1fce90cd1017e44a5069573724bf8
정해빈 <habin1213@naver.com>
2025-10-20 17:40:37 +0900

    chore: project backend init

---
+++ be-kakaon/src/main/java/com/s310/kakaon/KakaonApplication.java
@@ -0,0 +1,13 @@
+package com.s310.kakaon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakaonApplication.class, args);
+    }
+
+}
---
+++ be-kakaon/src/main/java/com/s310/kakaon/global/dto/PageInfo.java
@@ -0,0 +1,16 @@
+package com.s310.kakaon.global.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class PageInfo {
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+}
