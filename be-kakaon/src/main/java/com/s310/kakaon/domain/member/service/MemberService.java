8954a58bbed31c0ab4e9262c4166dc281db9f3a9
정해빈 <habin1213@naver.com>
2025-11-03 16:34:53 +0900

    feat: members api에 JWT 인증 적용

--- be-kakaon/src/main/java/com/s310/kakaon/domain/member/service/MemberService.java
+++ be-kakaon/src/main/java/com/s310/kakaon/domain/member/service/MemberService.java
@@ -8,9 +8,16 @@ public interface MemberService {
     /** 회원 단건 조회 */
     MemberResponseDto getMemberById(Long id);
 
+    /** providerId로 회원 조회 (JWT 인증용) */
+    MemberResponseDto getMemberByProviderId(String providerId);
+
     /** 회원 정보 수정 */
     MemberResponseDto updateMember(Long id, MemberUpdateRequestDto dto);
 
+    MemberResponseDto updateMemberByProviderId(String providerId, MemberUpdateRequestDto dto);
+
     /** 회원 탈퇴 */
     void softDeleteMember(Long id);
+
+    void softDeleteMemberByProviderId(String providerId);
 }
