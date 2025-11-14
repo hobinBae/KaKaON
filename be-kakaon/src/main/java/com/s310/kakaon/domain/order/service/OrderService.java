fde28b7613d73df2d4df828e4e4e4f35c584bf7e
배호빈 <qoghqls95@naver.com>
2025-11-14 09:52:12 +0900

    fix: self-invocation 문제 해결 및 트랜잭션 안정성을 위한 파사드 서비스 도입

--- be-kakaon/src/main/java/com/s310/kakaon/domain/order/service/OrderService.java
+++ be-kakaon/src/main/java/com/s310/kakaon/domain/order/service/OrderService.java
@@ -4,7 +4,6 @@ import com.s310.kakaon.domain.order.dto.*;
 import com.s310.kakaon.domain.order.entity.Orders;
 
 public interface OrderService {
-    OrderResponseDto createOrderAndPayment(Long memberId, Long storeId, OrderRequestDto request);
     Orders createOrder(Long memberId, Long storeId, OrderRequestDto request);
     OrderCancelResponseDto cancelOrder(Long memberId, Long storeId, Long orderId);
     OrderDetailResponseDto getOrderDetail(Long memberId, Long orderId);
